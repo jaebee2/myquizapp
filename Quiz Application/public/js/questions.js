2a02b451bd61513c7efb160552b15e07ba6a3c39
jibrilbulama <40578703@live.napier.ac.uk>
2022-04-28 16:06:03 +0100

    First

---
+++ Quiz Application/public/js/questions.js
@@ -0,0 +1,115 @@
+
+let questions = [
+  {
+  numb: 1,
+  question: "In the first avengers film, what was the name Tony Stark gave to Thor?",
+  answer: "Point Break",
+  options: [
+    "God of Thunder",
+    "lightning God",
+    "Asgardian",
+    "Point Break"
+  ]
+},
+  {
+  numb: 2,
+  question: "What is the real name of the Black Panther?",
+  answer: "T’Challa",
+  options: [
+    "N’Jobu",
+    "M’Baku",
+    "N’Jadaka",
+    "T’Challa"
+  ]
+},
+  {
+  numb: 3,
+  question: "What race of beings is loki from?",
+  answer: "Frost Giants",
+  options: [
+    "Dark elves",
+    "Frost Giants",
+    "Gods",
+    "Asgardians"
+  ]
+},
+  {
+  numb: 4,
+  question: "What is the name of Thor’s hammer?",
+  answer: "Mjolnier",
+  options: [
+    "Mjolnier",
+    "Vanir",
+    "Stormbreaker",
+    "Odinsward"
+  ]
+},
+  {
+  numb: 5,
+  question: "What’s the name of the mysterious blue glowing cube that Loki uses as a weapon?",
+  answer: "Tesseract",
+  options: [
+    "The Orb",
+    "The scepter",
+    "Tesseract",
+    "Space Stone"
+  ]
+},
+
+
+  {
+   numb: 6,
+   question: "Who is Carol Danvers?",
+   answer: "Captain Marvel",
+   options: [
+     "Thor’s Girlfriend",
+     "Shuri",
+     "Captain Marvel",
+     "Nebula"
+   ]
+ },
+{
+     numb: 7,
+     question: "Which of this Avengers was not present at the battle of wakanda ?",
+     answer: "Hulk",
+     options: [
+       "Hulk",
+       "Captain America",
+       "Falcon",
+       "Thor"
+     ]
+   },
+  {
+       numb: 8,
+       question: "Who took out Tony Stark’s parents?",
+      answer: "Winter Soldier",
+      options: [
+         "Thanos",
+         "Winter Soldier",
+         "Loki",
+         "Ultron"
+       ]
+     },
+    {
+         numb: 9,
+         question: "What is the signature move of Black Panther?",
+         answer: "The Panter Punch",
+         options: [
+           "The Vibranium Strike",
+           "The Panter Strike",
+           "The Vibranium Blast",
+           "The Panter Punch"
+         ]
+        },
+      {
+           numb: 10,
+          question: "In which Avengers movie does Scarlet Witch make her appearance?",
+           answer: "Avengers Age of Ultron",
+           options: [
+             "Captain america civil war",
+             "Avengers endgame",
+             "Avengers Age of Ultron",
+             "Avengers Assemble"
+           ]
+         },
+];
